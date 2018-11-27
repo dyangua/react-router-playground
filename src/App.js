5af300c6d5e203acb41be7e65892d20c761b96b4
dyangua <dannyyanguacorrea@hotmail.com>
2018-11-26 22:01:25 -0500

    pass props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
+
+const routes = [];
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Playground</h1>
+        <h2>Avoid to use component in Route to pass props in a component</h2>
+
+        <pre>
+          {routes.map(({ path, component, MyComponent, Initialdata }) => (
+            <Route
+              path={path}
+              render={props => (
+                <MyComponent {...props} Initialdata={Initialdata} />
+              )}
+            />
+          ))}
+        </pre>
       </div>
     );
   }
