b37b53199078ee251f77a82336fe3a43c52de290
dyangua <dannyyanguacorrea@hotmail.com>
2018-11-26 22:43:54 -0500

    two approch Redirect and use history

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Redirect, withRouter } from "react-router-dom";
 
-class App extends Component {
+// Component Composition
+// Declarative UI
+// event -> state change -> re-render
+
+const fakeAPi = new Promise();
+
+class Login extends Component {
+  state = {
+    toDashBoard: false
+  };
+
+  handleSubmit = user => {
+    //  do some stuff with api ... then() {
+    this.setState({
+      toDashBoard: true
+    });
+    // }
+  };
+
+  handleSubmitHistory = user => {
+    //  do some stuff with api ... then() {
+    fakeAPi().then(() => this.props.history.push("/dashboard"));
+    // }
+  };
+
   render() {
+    if (this.state.toDashBoard === true) {
+      return <Redirect to="/dashboard" />;
+    }
+
     return (
       <div>
-        <h1>Playground</h1>
+        <h1>Log in</h1>
+        <form onSubmit={this.handleSubmit} />
       </div>
     );
   }
 }
 
-export default App;
+// export default Login;
+export default withRouter(Login);
